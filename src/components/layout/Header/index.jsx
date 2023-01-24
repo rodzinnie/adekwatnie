462048efc1bd9d2669fbeffe3364cfac2b55e5a3
agiskorska <skorskaga@gmail.com>
2023-01-24 23:13:03 +0000

    Update header

--- src/components/layout/Header/index.jsx
+++ src/components/layout/Header/index.jsx
@@ -1,8 +1,10 @@
 import styles from './index.module.scss'
-
+import {Navbar} from '../../common'
 function Header() {
   return (
-    <div className={styles.root}>Header</div>
+    <div className={styles.root}>
+      <Navbar />
+    </div>
   )
 }
 
