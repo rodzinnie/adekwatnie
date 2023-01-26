634f6cc51c1cc4b421513e79f38935a64734b67b
agiskorska <skorskaga@gmail.com>
2023-01-26 23:40:21 +0000

    Add changes to offer - display tiles

--- src/components/layout/Offer/index.jsx
+++ src/components/layout/Offer/index.jsx
@@ -5,26 +5,32 @@ const offerList = [
   {
     title: 'Audiodeskrypcje Filmowe',
     text: 'Masz gotowy tekst AD, ale potrzebujesz go nagrać? Współpracuję z profesjonalnymi studiami w Krakowie i Warszawie. Nagrywam głównie audiodeskrypcje do filmów, ale nie tylko.',
+    variant: 'darkBlue' , fill: '#1d3557', buttons: ['play']
   },
   {
     title: 'Audiodeskrypcje Teatralne',
     text: '1Masz gotowy tekst AD, ale potrzebujesz go nagrać? Współpracuję z profesjonalnymi studiami w Krakowie i Warszawie. Nagrywam głównie audiodeskrypcje do filmów, ale nie tylko.',
+    variant: 'darkBlue' , fill: '#1d3557', buttons: ['play']
   },
   {
     title: 'Audiodeskrypcje Eventów',
     text: '2Masz gotowy tekst AD, ale potrzebujesz go nagrać? Współpracuję z profesjonalnymi studiami w Krakowie i Warszawie. Nagrywam głównie audiodeskrypcje do filmów, ale nie tylko.',
+    variant: 'darkBlue' , fill: '#1d3557', buttons: ['play']
   },
   {
     title: 'Nagrywanie Ścieżek',
     text: '3Masz gotowy tekst AD, ale potrzebujesz go nagrać? Współpracuję z profesjonalnymi studiami w Krakowie i Warszawie. Nagrywam głównie audiodeskrypcje do filmów, ale nie tylko.',
+    variant: 'darkBlue' , fill: '#1d3557', buttons: ['play']
   },
   {
     title: 'Voiceover - lektorka na żywo',
     text: '4Masz gotowy tekst AD, ale potrzebujesz go nagrać? Współpracuję z profesjonalnymi studiami w Krakowie i Warszawie. Nagrywam głównie audiodeskrypcje do filmów, ale nie tylko.',
+    variant: 'darkBlue' , fill: '#1d3557', buttons: ['play']
   },
   {
     title: 'Szkolenia',
     text: '5Masz gotowy tekst AD, ale potrzebujesz go nagrać? Współpracuję z profesjonalnymi studiami w Krakowie i Warszawie. Nagrywam głównie audiodeskrypcje do filmów, ale nie tylko.',
+    variant: 'darkBlue' , fill: '#1d3557', buttons: ['play']
   },
 ]
 
