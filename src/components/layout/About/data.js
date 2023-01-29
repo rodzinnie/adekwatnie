7bc344c09e05de93bfba1138d9b96e0687a432db
agiskorska <skorskaga@gmail.com>
2023-01-29 23:01:14 +0000

    Import data; Fix imports

---
+++ src/components/layout/About/data.js
@@ -0,0 +1,33 @@
+export const list = [
+  {
+    title: 'Próbka Głosu 1',
+    buttons: [
+      { name: 'play', fill: '#e63946' },
+      { name: 'stop', fill: '#e63946' },
+    ],
+    variant: 'darkBlue',
+  },
+  {
+    title: 'Próbka Głosu 2',
+    buttons: [
+      { name: 'play', fill: '#e63946' },
+      { name: 'stop', fill: '#e63946' },
+    ],
+    variant: 'lightBlue',
+  },
+  {
+    title: 'Próbka Głosu 3',
+    buttons: [
+      { name: 'play', fill: '#e63946' },
+      { name: 'stop', fill: '#e63946' },
+    ],
+    variant: 'red',
+  },
+]
+
+export const testimonials = [
+  'Obejrzałam ten film, ale powiem, że bez szczególnych emocji. Nic mnie w tym filmie zbytnio nie poruszyło. Jedynym pozytywem dla mnie jest bardzo dobrze zrobiona i przeczytana audiodeskrypcja, bo bez niej chyba nie dotrwałabym do końca filmu (H.U., recenzja filmu Śniegu już nigdy nie będzie)',
+  'Audiodeskrypcja sporządzona i przeczytana na najwyższym poziomie. Brawo (U.N., recenzja filmu Śniegu już nigdy nie będzie)',
+  'W jak widoki – są piękne, co znakomicie oddała bardzo dobra audiodeskrypcja (E.G., recenzja filmu Psie pazury)',
+  'Jeśli mowa o postaciach, to Phil od początku filmu wydawał mi się zły – czy tego chciała autorka, żebyśmy patrzyli ze złością na tego kowboja i myśleli „on coś kombinuje coś złego?. Dużo dawała też barwna, a jednocześnie informująca o przebiegu akcji audiodeskrypcja…” (J.M., recenzja filmu Psie pazury)',
+]
