3bce69c0ca185e762f15d20340b1c51696013b15
agiskorska <skorskaga@gmail.com>
2023-01-30 22:18:23 +0000

    Footer MVP

--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -2,7 +2,11 @@ import styles from './index.module.scss'
 
 function Footer() {
   return (
-    <div className={styles.root}>Footer</div>
+    <div className={styles.root}>
+      <a href="https://aggieskorska.co.uk" className={styles.link}>
+        created and maintained by skinny&co. a.d. 2023
+      </a>
+    </div>
   )
 }
 
