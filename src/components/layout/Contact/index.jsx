b2e399673acaa4e144b447a6668c260ab1988ce4
agiskorska <skorskaga@gmail.com>
2023-02-13 22:37:26 +0000

    Add TODO

--- src/components/layout/Contact/index.jsx
+++ src/components/layout/Contact/index.jsx
@@ -3,6 +3,7 @@ import styles from './index.module.scss'
 
 function Contact() {
   const mailto = (action) => {
+    //TODO this will have to come from context (data.js)
     const actionToTake = action=='mail' ? 'mailto:contact@test.com' : 'tel:782655969'
     const a = document.createElement('a')
     a.href = actionToTake;
