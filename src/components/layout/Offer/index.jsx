11ce09a7dfe1fe74ffc6d6156d7a8d74c0a1a93c
agiskorska <skorskaga@gmail.com>
2023-03-18 21:41:17 +0000

    Small fixes

--- src/components/layout/Offer/index.jsx
+++ src/components/layout/Offer/index.jsx
@@ -39,7 +39,7 @@ function Offer() {
       <div className={styles.bgRed} id='readMore'>
         <ContrastSection className={styles.container}>
           <div className={styles.spread}>
-            <Heading variant={'bgRed'} headingLevel='3' text={currentSlide.title} />
+            <Heading variant={'bgBlue'} headingLevel='5' text={currentSlide.title} />
             <div className={styles.aboutMe}>
               <h6>{currentSlide.text}</h6>
             </div>
